862d57cedf782d6e112e9a7a60a09bed5fedd510
Jinyin Chai <cjy990324>
2022-11-03 13:27:48 -0700

    a7

---
+++ src/tuiter/editProfile/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import EditProfile from "./EditProfile";
+import myProfile from "../data/profile.json"
+import {useSelector} from "react-redux";
+
+const EditProfileComponent = () => {
+    const myProfileArray = useSelector((state) => state.myProfile);
+
+    return(
+        <>
+            {
+                myProfileArray.map(myProfile => {
+                    return(
+                        <EditProfile myProfile={myProfile} key={myProfile._id}/>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+export default EditProfileComponent;
